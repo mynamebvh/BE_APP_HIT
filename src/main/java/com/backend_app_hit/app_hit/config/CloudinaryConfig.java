185e6b1100e108bda38fb58e99e508e4e02fc7e0
mynamebvh <mynamebvh@gmail.com>
2021-06-12 03:11:36 +0700

    api upload img

---
+++ src/main/java/com/backend_app_hit/app_hit/config/CloudinaryConfig.java
@@ -0,0 +1,24 @@
+package com.backend_app_hit.app_hit.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.cloudinary.Cloudinary;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+  @Bean
+  public Cloudinary config() {
+    Map<String,String> config = new HashMap<>();
+    config.put("cloud_name", "dhlmdhzbz");
+    config.put("api_key", "");
+    config.put("api_secret", "");
+    Cloudinary cloudinary = new Cloudinary(config);
+    return cloudinary;
+  }
+
+}
